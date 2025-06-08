e7936cfa1672044b17130bf2bec6fcdece58cbd8
yunseo <choijang1117@hanmail.net>
2025-06-09 02:28:57 +0900

    28. ISP: Interface Segregation Principle

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -8,6 +8,7 @@ public class GameApplication {
         GameLevel gameLevel = new VeryBeginner();
 
         Minesweeper minesweeper = new Minesweeper(gameLevel);
+        minesweeper.initialize();
         minesweeper.run();
     }
 
